9c410dad92a08803613735e227fe3256a6b3c83e
Ammar Ashfaq <ammar_ashfaq@yahoo.com>
2018-01-30 09:05:23 -0800

    change

---
+++ src/main/java/com/javawebp/JavawebpApplication.java
@@ -0,0 +1,12 @@
+package com.javawebp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebpApplication.class, args);
+	}
+}
